a5fee5c157c47c82d3a167fce80468ac0912dace
Ayo Omotosho <aomotosho4@gmail.com>
2023-07-12 11:03:46 -0500

    fixed chat functionality and database error

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,16 +1,20 @@
 import { getAuth } from "firebase/auth";
 import {
   Firestore,
+  getDoc,
   setDoc,
   getDocs,
   collection,
   doc,
   onSnapshot,
+  where,
   addDoc,
   orderBy,
   query,
   serverTimestamp,
   updateDoc,
+  Timestamp,
+  arrayUnion,
 } from "firebase/firestore";
 import React, {
   useCallback,
@@ -21,6 +25,7 @@ import React, {
 import { Image, SafeAreaView, Text, View } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
 import { FIREBASE_AUTH, FIREBASE_DB } from "../firebaseConfig";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Chat({ route }) {
   const [messages, setMessages] = useState([]);
@@ -30,128 +35,50 @@ export default function Chat({ route }) {
       ? uid + FIREBASE_AUTH.currentUser.uid
       : FIREBASE_AUTH.currentUser.uid + uid;
 
-  const getAllMessages = async () => {
-    const msgRef = collection(FIREBASE_DB, "chats");
-    const q = query(msgRef, orderBy("createdAt", "desc"));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.data().text);
-      setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, doc.data())
-      );
-    });
-  };
-
   useLayoutEffect(() => {
-    // const q = query(
-    //   collection(FIREBASE_DB, "solo_chats"),
-    //   orderBy("messages.createdAt", "desc")
-    // );
-    //
-    // // listen to changes in the collection
-    // const unsubscribe = onSnapshot(q, (snapshot) =>
-    //   setMessages(
-    //     snapshot.docs.map((doc) => ({
-    //       _id: doc.data()._id,
-    //       createdAt: doc.data().createdAt,
-    //       text: doc.data().text,
-    //       user: doc.data().user,
-    //     }))
-    //   )
-    // );
-    //
-    //
-    // const q = query(
-    //   collection(FIREBASE_DB, "solo_chats"),
-    //   orderBy("createdAt", "desc")
-    // );
+    const chatRef = collection(FIREBASE_DB, "chats");
+    const q = query(chatRef, orderBy("createdAt", "desc"));
 
-    // const unsubscribe = onSnapshot(q, (snapshot) =>
-    //   setMessages(
-    //     snapshot.docs.map((doc) => ({
-    //       _id: doc.data()._id,
-    //       createdAt: doc.data().createdAt.toDate(),
-    //       text: doc.data().text,
-    //       user: doc.data().user,
-    //     }))
-    //   )
-    // );
+    //whenever this runs it means that a new message has been added to the database
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setMessages(
+        querySnapshot.docs.map((doc) => ({
+          _id: doc.data()._id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user,
+        }))
+      );
 
-    // const unsub = onSnapshot(doc(FIREBASE_DB, "chats", chatId), (snapshot) => {
-    //   snapshot.docs.map((doc) => {
-    //     console.log(doc.data());
-    //   });
-    //   // setMessages(
-    //   snapshot.data().messages.map((msg) => ({
-    //     _id: msg.chatId,
-    //     createdAt: msg.createdAt.toDate(),
-    //     text: msg.text,
-    //     user: msg.user,
-    //   }))
-    // );
-    // });
-
-    return () => getAllMessages();
+      return () => unsubscribe();
+    });
   }, []);
 
-  const onSend = useCallback((messages = []) => {
-    setMessages((previousMessages) => {
-      GiftedChat.append(previousMessages, messages);
-    });
-    console.log("messages", messages);
-    const userMsg = {
-      ...messages[0],
+  // TODO: FIGURE OUT HOW TO ADD MESSAGE TO MESSAGE ARRAY IN FIREBASE AND THE DISPLAY MESSAGES ONLY TO THE CORRECT CURRENT AND OTHER USER
+  //useCallback is a hook that will prevent the function from being recreated every time the component re-renders
+  const onSend = useCallback((sentMessage = []) => {
+    // getMessages();
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, sentMessage)
+    );
+
+    const { _id, createdAt, text, user } = sentMessage[0];
+
+    addDoc(collection(FIREBASE_DB, "chats"), {
+      _id,
+      chatId: chatId,
       sentBy: FIREBASE_AUTH.currentUser.uid,
       sentTo: uid,
-      createdAt: new Date(),
-    };
-    const { id, createdAt, text, user } = messages[0];
-    addDoc(collection(FIREBASE_DB, "chats"), {
-      chatId,
       createdAt,
-      ...userMsg,
+      text,
       user,
     });
   }, []);
 
-  useEffect(() => {
-    getAllMessages();
-  }, []);
-
-  // const onSend = async (msgArray) => {
-  //   const msg = msgArray[0];
-  //   console.log("msgArray", msgArray);
-  //   console.log("msg", msg);
-  //   const userMsg = {
-  //     ...msg,
-  //     sentBy: FIREBASE_AUTH.currentUser.uid,
-  //     sentTo: uid,
-  //     createdAt: serverTimestamp(),
-  //   };
-  //
-  //   setMessages((previousMessages) => {
-  //     GiftedChat.append(previousMessages, messages);
-  //   });
-  //
-  //   const combinedId =
-  //     uid > FIREBASE_AUTH.currentUser.uid
-  //       ? uid + FIREBASE_AUTH.currentUser.uid
-  //       : FIREBASE_AUTH.currentUser.uid + uid;
-  //
-  //   await setDoc(doc(FIREBASE_DB, "solo_chats", combinedId), {
-  //     messages: userMsg,
-  //   });
-  //
-  //   // setDoc(doc(FIREBASE_DB, "solo_chats", uid), {
-  //   //   _id: combinedId,
-  //   //   messages: userMsg,
-  //   // });
-  // };
-
   return (
     <SafeAreaView style={{ width: "100%", height: "100%" }}>
       <GiftedChat
-        messages={messages.reverse()}
+        messages={messages}
         showAvatarForEveryMessage={true}
         onSend={(messages) => onSend(messages)}
         user={{
