6613717526d36adbe51747d3e752e134df9fb428
Ayo Omotosho <aomotosho4@gmail.com>
2023-07-16 13:40:04 -0500

    remove unused imports

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
@@ -9,24 +8,6 @@ import Chat from "./screens/Chat";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
-  /*
-   *         <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Sign Up"
-          component={SignUpScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-   * */
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
